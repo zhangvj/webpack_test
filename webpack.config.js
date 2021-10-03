921b76775ef79411cf5d9327105d031319a17630
zhangvj <zhangvj1995@163.com>
2021-10-03 20:57:33 +0800

    feat: 开发环境测试

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出'
+      title: 'development'
     })
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    publicPath: '/'
   }
 }
