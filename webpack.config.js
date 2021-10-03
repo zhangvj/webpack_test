36cd586402051a17b55f1fbe26ee1d95eb655d63
zhangvj <zhangvj1995@163.com>
2021-10-03 11:14:20 +0800

    chore: 删除测试代码

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
-    ]
   }
 }
